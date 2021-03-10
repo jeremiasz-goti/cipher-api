5189c73a1b8bf46e199227e7e798ed0dc4b3bef6
jeremiasz-goti <jeremiasz.goti@gmail.com>
2021-03-10 22:28:38 +0100

    Better implementation of cipher

--- cipher.py
+++ cipher.py
@@ -3,18 +3,84 @@
 Caesar Cipher implementation
 
 """
+import string
 
 def encode(to_encode, shift):
     encrypted = ""
-    for i in to_encode:
-        encrypted += chr(ord(i) + shift)
+    whitespaces = [ ord(x) for x in string.whitespace ]
+    punctuation_chars = [ ord(x) for x in string.punctuation ]
+    
+    for char in to_encode:
+        ascii_char = ord(char)
+       
+
+        if ascii_char in range(65,90):
+            char_uni = ord(char) - 65
+            char_uni += shift
+            char_uni = char_uni % 26
+            encrypted += chr(char_uni + 65)
+
+        elif ascii_char in range(97,123):
+            char_uni = ord(char) - 97
+            char_uni += shift
+            char_uni = char_uni % 26
+            encrypted += chr(char_uni + 97)
+        
+        elif ascii_char in range(48,57):
+            char_uni = ord(char) - 48
+            char_uni += shift
+            char_uni = char_uni % 10
+            encrypted += chr(char_uni + 48)
+
+        elif ascii_char in whitespaces:
+            encrypted += char
+        
+        elif ascii_char in punctuation_chars:
+            encrypted += char
+
     return encrypted
 
 def decode(to_decode, shift):
     decrypted = ""
-    for i in to_decode:
-        decrypted += chr(ord(i) - shift)
+    whitespaces = [ ord(x) for x in string.whitespace ]
+    punctuation_chars = [ ord(x) for x in string.punctuation ]
+    
+    for char in to_decode:
+        ascii_char = ord(char)
+       
+
+        if ascii_char in range(65,90):
+            char_uni = ord(char) - 65
+            char_uni -= shift
+            char_uni = char_uni % 26
+            decrypted += chr(char_uni + 65)
+
+        elif ascii_char in range(97,123):
+            char_uni = ord(char) - 97
+            char_uni -= shift
+            char_uni = char_uni % 26
+            decrypted += chr(char_uni + 97)
+        
+        elif ascii_char in range(48,57):
+            char_uni = ord(char) - 48
+            char_uni -= shift
+            char_uni = char_uni % 10
+            decrypted += chr(char_uni + 48)
+
+        elif ascii_char in whitespaces:
+            decrypted += char
+        
+        elif ascii_char in punctuation_chars:
+            decrypted += char
+
     return decrypted
 
-print(encode("abcd fdsa 123",3))
-print(decode("defg#igvd#456", 3))
+
+print(encode("Jeremiasz kuchaRski 123! {", 3))
+print(decode("Mhuhpldvc nxfkdUvnl 456! {", 3))
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-import secrets
 from fastapi import FastAPI, Depends, status, HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from cipher import encode, decode
-
+from api_auth import user_auth
 
 app = FastAPI()
-security = HTTPBasic()
-
-def user_auth(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "development")
-    correct_password = secrets.compare_digest(credentials.password, "development")
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
-
 
 @app.get("/encode/msg={to_encode}&shift={shift}")
 async def enc(to_encode: str, shift: int,username: str = Depends(user_auth)):
